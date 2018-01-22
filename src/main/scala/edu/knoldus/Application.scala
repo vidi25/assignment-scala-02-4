b0d27f1ac11f0c40ae4fd1b588493993c5bc546f
vidisha <vidisha.gupta@knoldus.in>
2018-01-22 11:25:06 +0530

    Updated main method

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -12,13 +12,16 @@ object Application extends App {
   val commValue4 = 6
   val clientComm = ClientSideCommission(commValue1)
   val clientComm1 = ClientSideCommission(commValue2)
-  val inputList = List(clientComm, clientComm1)
-  val totalComm = new TotalCommission[ClientSideCommission](inputList)
+  val listOfClientCommission = List(clientComm, clientComm1)
+  val totalComm = new TotalCommission[ClientSideCommission](listOfClientCommission)
   log.info(s"${totalComm.getTotalCommission}\n")
   val streetComm = StreetSideCommission(commValue3)
   val streetComm1 = StreetSideCommission(commValue4)
-  val inputList1 = List(streetComm, streetComm1)
-  val totalComm1 = new TotalCommission[StreetSideCommission](inputList1)
+  val listOfStreetCommission = List(streetComm, streetComm1)
+  val totalComm1 = new TotalCommission[StreetSideCommission](listOfStreetCommission)
   log.info(s"${totalComm1.getTotalCommission}\n")
+  val listOfMingledCommission = List(clientComm, streetComm)
+  val totalComm2 = new TotalCommission[Commission](listOfMingledCommission)
+  log.info(s"${totalComm2.getTotalCommission}")
 
 }
